fa60a40cd005feffb5b9d153a2c30d0b5b94e832
Magnus <mag.aars@gmail.com>
2015-03-12 18:51:21 +0100

    added folder LSFEM to the Poisson folder with a LSFEM implementation, needs testing

---
+++ Poisson/LSFEM/load_2D.m
@@ -0,0 +1,46 @@
+function [fh] = load_2D(dofs,p,tri,f)
+% function [fh] = load_2D(dofs,p,tri,f),
+% 
+% description:
+%     Evaluates the loading function f in all the nodes.
+%
+% arguments:
+%		- f 		Loading function expression of two variables
+%   - dofs  Degrees of freedom. 
+%   - p     Nodal points. (x,y)-coordinates for point i given in row i.
+%   - tri   Elements. Index to the three corners of element i given in row i.
+% returns:
+%   - fh 		The loading function evaluated in all the nodes.
+%
+% author: Magnus Aa. Rud
+% last edit: March 2015
+
+    fh = zeros(dofs,1);
+    Nq = 4;
+    Ne = length(tri(:,1)); %Number of elements
+    
+    for i = 1:Ne
+        pis = tri(i,:);
+        p1 = p(pis(1),:);
+        p2 = p(pis(2),:);
+        p3 = p(pis(3),:);
+
+        A_k = abs((p1(1)-p3(1))*(p2(2)-p1(2)) - (p1(1)-p2(1))*(p3(2)-p1(2))); % Jacobian
+
+				Map = reshape([3*(pis-1)+1;3*(pis-1)+2;3*pis],1,9); % Map is now a vector containing the corresponding global indices
+        
+        delPhi = zeros(2,3);
+        delPhi(1,1) = p2(2)-p3(2);
+        delPhi(2,1) = p3(1)-p2(1);
+        delPhi(1,2) = p3(2)-p1(2);
+        delPhi(2,2) = p1(1)-p3(1);
+        delPhi(1,3) = p1(2)-p2(2);
+        delPhi(2,3) = p2(1)-p1(1);
+
+				M = [delPhi;0 0 0];	
+        
+				I = quadrature2D(p1,p2,p3,Nq,f);
+				fh(Map) = fh(Map) + I*reshape(M,9,1);       
+    end
+end
+
---
+++ Poisson/LSFEM/main.m
@@ -0,0 +1,40 @@
+% main.m
+%
+% description:
+%      Solving the Poisson problem on the square (0,1)^2;
+%
+% author: Magnus Aa. Rud
+% last edit: March 2015
+
+N = 30; % Number of Nodes in each direction.
+dofs = 3*N^2; % Number of degrees of freedom.
+[p,tri,e] = getSquare(N); %nodes, edges and elements.
+f = @(x,y) x-y; % Loading function
+%f=@(x,y) -8*pi*cos(2*pi*(x^2+y^2))+16*pi^2*(x^2+y^2)*sin(2*pi*(x^2+y^2)); %Loading function
+
+% Assemble Matrices and loading function
+	Ah = stiffness_2D(dofs,p,tri);
+	fh = load_2D(dofs,p,tri,f);
+% 
+
+
+% Imposing Homogenous boundary conditions 
+for j = e
+	i = 3*j;	
+  Ah(i,:) = 0;
+  Ah(:,i) = 0;
+  Ah(i,i) = 1;
+  fh(i) = 0;
+end
+
+%
+%
+
+% Solving 
+uh = Ah\fh;
+	
+%% Plotting the numerical solution
+figure(1);
+trisurf(tri,p(:,1),p(:,2),uh(3:3:dofs));
+title('Numerical Solution');
+
