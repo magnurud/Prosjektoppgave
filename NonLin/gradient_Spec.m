fe8b80b1d513fd305d465e4898d74081924d396c
Magnus <mag.aars@gmail.com>
2015-05-06 08:44:18 +0200

    minor change in grad_Spec.m

--- NonLin/gradient_Spec.m
+++ NonLin/gradient_Spec.m
@@ -5,9 +5,9 @@ function [Gh] = gradient_Spec(LDM,B1,B2,W,dofs);
 % arguments:
 %   - LDM   The derivatives of the basisfunctions evaluated in every point
 %           ( LDM(i,j) = l_j'(x_i) )
-%   - B1     The N by N matrix containing the first component of the vectorfield B evaluated in each
+%   - B1     The diagonal matrix dofs by dofs matrix containing the first component of the vectorfield B evaluated in each
 %   (x,y)
-%   - B2     The N by N matrix containing the first component of the vectorfield B evaluated in each
+%   - B2     The diagonal dofs by dofs matrix containing the second component of the vectorfield B evaluated in each
 %   (x,y)
 %   - W     The GLL-weights
 %   - dofs  degrees of freedom 
@@ -17,8 +17,8 @@ function [Gh] = gradient_Spec(LDM,B1,B2,W,dofs);
 % author: Magnus Aa. Rud
 % last edit: April 2015
 
-Bd1 = diag(reshape(B1,dofs,1)); 
-Bd2 = diag(reshape(B2,dofs,1)); 
+%Bd1 = diag(reshape(B1,dofs,1)); 
+%Bd2 = diag(reshape(B2,dofs,1)); 
 
-Gh = Bd1*kron(W,W*LDM) + Bd2*kron(W*LDM,W);
+Gh = B1*kron(W,W*LDM) + B2*kron(W*LDM,W);
 
